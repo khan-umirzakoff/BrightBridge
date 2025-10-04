c9339a571d8df3e7d509db851ef68e504599c7c3
khanumirzakoff <khanumirzakoff@gmail.com>
2025-10-04 11:50:25 +0000

    1

---
+++ .idx/dev.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+
+let
+  system = builtins.currentSystem;
+
+  old = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/73bc3300ad02be21998a7c0e987592ca66df73f3.tar.gz";
+    # Tavsiya: reproducible bo'lishi uchun bu yerga sha256 qo'shing.
+  }) {};
+
+  phpPkg = if builtins.hasAttr "php74" old then old.php74 else old.php;
+  composerPkg = if builtins.hasAttr "php74Packages" old then old.php74Packages.composer else (old.phpPackages.composer or old.composer);
+
+in
+{
+  packages = [
+    phpPkg
+    composerPkg
+  ];
+}
